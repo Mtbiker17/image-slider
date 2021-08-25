8d793cf6d83e8425478bdaedaf7c2893357f9429
Devin <ospreywebservices@gmail.com>
2021-08-25 18:15:27 -0400

    created function for showing slides on a timer

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let imageIndex = 1;
+let imageNumber = 0;
+showSlider();
 
-function showCurrent(imageIndex) {
+function showSlider() {
+    let images = document.getElementsByClassName('images');
+    for(i = 0; i < images.length; i++) {
+        images[i].style.display ='none';
+    }
+    imageNumber++;
+    if(imageNumber > images.length) {
+        imageNumber = 1;
+    }
+    images[imageNumber - 1].style.display = 'block';
+    setTimeout(showSlider, 2000);
+}
+
+function next() {
 
 }
 
-function next(imageIndex) {
-    imageIndex++
-}
-
-function previous(imageIndex) {
+function previous() {
 
 }
