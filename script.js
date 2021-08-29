2617f5f1a863cae10845f846568f4267cda666e4
Devin <ospreywebservices@gmail.com>
2021-08-29 16:29:03 -0400

    added next and previous functions for slide change, bugged for manual slide change currently

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-let imageNumber = 0;
-showSlider();
+let nextBtn = document.getElementById('nextBtn');
+let prevBtn = document.getElementById('previousBtn');
+let imageNumber = 1;
+let timer;
+
+
+const sliderController = (() => {
+let images = document.getElementsByClassName('images');
 
 function showSlider() {
-    let images = document.getElementsByClassName('images');
+    
     for(i = 0; i < images.length; i++) {
         images[i].style.display ='none';
     }
-    imageNumber++;
+    
     if(imageNumber > images.length) {
         imageNumber = 1;
     }
+    if(imageNumber < 1) {
+        imageNumber = 4;
+    }
     images[imageNumber - 1].style.display = 'block';
-    setTimeout(showSlider, 1000);
+    timer = setTimeout(showSlider, 2000);
+    imageNumber++
 }
 
 function next() {
-
-}
+    clearTimeout(timer);
+    imageNumber++;
+    timer = setTimeout(showSlider, 2000);
+};
 
 function previous() {
-
+    imageNumber = imageNumber -1;
 }
+
+return {
+    showSlider,
+    next,
+    previous
+}
+
+})();
+
+sliderController.showSlider();
+
+
+nextBtn.addEventListener('click', () => {
+    sliderController.next();
+})
+
+prevBtn.addEventListener('click', () => {
+    sliderController.previous();
+})
+
+
+
+
